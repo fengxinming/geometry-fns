ae93ad3cffa2e6e72d4945a30fceeb688c3afd42
Jesse Feng <fxm0016@126.com>
2021-05-20 09:56:38 +0800

    Create distanceBetween.js

---
+++ src/distanceBetween.js
@@ -0,0 +1,12 @@
+/**
+ * 计算两点之间的距离
+ *
+ * @param {number} x1
+ * @param {number} y1
+ * @param {number} x2
+ * @param {number} y2
+ * @returns {number}
+ */
+export default function distanceOf(x1, y1, x2, y2) {
+  return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+}

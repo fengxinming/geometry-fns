f67e55c031b2e6592f97618d559bb9243103199a
Jesse Feng <fxm0016@126.com>
2021-05-20 10:00:43 +0800

    Create index.d.ts

---
+++ types/index.d.ts
@@ -0,0 +1,35 @@
+export declare function areIntersected(
+  qx1: number, 
+  qy1: number, 
+  qx2: number, 
+  qy2: number, 
+  lx1: number, 
+  ly1: number, 
+  lx2: number, 
+  ly2: number
+): boolean
+
+export declare function crossOverExperiments(
+  lx1: number, 
+  ly1: number, 
+  lx2: number, 
+  ly2: number, 
+  x1: number, 
+  y1: number
+): number
+
+export declare function distanceOf(
+  x1: number,
+  y1: number, 
+  x2: number, 
+  y2: number
+): number
+
+export declare function isConvexPolygon(polygon: number[]): boolean
+
+export declare function lineCrossedQuadrangle(
+  line: number[], 
+  quadrangle: number[]
+): [number[], number[]] | null
+
+export declare function orthocenterOf(polygon: number[]): number[] | null
